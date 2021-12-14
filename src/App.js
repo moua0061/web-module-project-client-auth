0bfdf36c805bdcf331ecb25e8e26b3b62de634a6
Lia Moua <moua0061@yahoo.com>
2021-12-14 16:21:43 -0500

    built out my form login and links

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <header>
+        <h1>Friends Database</h1>
+        <Link to='/login'>Login</Link>
+        <Link to='/login'>Friend List</Link>
+        <Link to='/login'>Add Friend</Link>
+        <Link to='/login'>Logout</Link>
+      </header>
+      <h1>Login</h1>
+      <form>
+        <label>Username &nbsp;</label>
+        <input />
+        <label>Password &nbsp;</label>
+        <input />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
